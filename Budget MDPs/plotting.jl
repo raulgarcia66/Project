a7573eaddc9a1fd77a15ebb257e63acfac5284bf
Raul Garcia <rauljgarcia66@gmail.com>
2023-04-02 21:44:29 -0500

    Fixed plots

--- Budget MDPs/plotting.jl
+++ Budget MDPs/plotting.jl
@@ -1,16 +1,18 @@
 using Plots
 
-function plot_V(i, t, B, v)
-    # B: (action => budget) vector
-    # v: (action => value) vector
+function plot_V(i, t, B, v; save_plot=false)
+    # B: useful budgets
+    # v: values
 
-    B = map(b -> b.second, B)
-    v = map(val -> val.second, v)
-
-    plot(title="V(s = $i; t = $t)", xlabel="Budget", legend=false)
+    println("B: $B \nv = $v\n")
+    plot(title="V(s = $i; t = $t)", xlabel="Budget", ylims = (-0.05 * maximum(v), 1.05 * maximum(v)), legend=false)  # assuming minimum(v) = 0
     for i in 1:(length(B)-1)
-        plot!([B[i]; B[i+1]], [v[i]; v[i]], ls = :dash)
+        plot!([B[i]; B[i+1]], [v[i]; v[i]]) # ls = :dash)
         scatter!([B[i]], [v[i]])
     end
     display(scatter!([B[end]], [v[end]]))
+
+    if save_plot
+        png("V at i $i t $t")
+    end
 end
