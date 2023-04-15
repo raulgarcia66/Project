946c4b313ba2da30db610eb0be611746549f8291
Raul Garcia <rauljgarcia66@gmail.com>
2023-04-14 20:23:45 -0500

    Update with new functions

--- Budget MDPs/test.jl
+++ Budget MDPs/test.jl
@@ -1,4 +1,5 @@
 using LinearAlgebra
+
 using JuMP
 using Gurobi
 import Random
@@ -40,38 +41,78 @@ Random.seed!(1)
 # Run tests
 sub = 1
 
-B_union_vec, BB_vec, B_tilde_union_vec, v_union_vec, vv_vec, v_tilde_union_vec, B_vec, B_tilde_vec, v_vec, v_tilde_vec = compute_useful_budgets(states, actions, B_max, P[sub], C[sub], R[sub], Γ[sub], T);
+B_union_vec, BB_vec, v_union_vec, vv_vec = compute_deterministic_data(states, actions, B_max, P[sub], C[sub], R[sub], Γ[sub], T);
 
-t = 1
 state = 1
-b = 4.5
 a = 1
+b = 4.5
+t = 1
 
 act, bud, val = get_action_budget_value_det(b, state, B_union_vec, v_union_vec, t)
 val = V_function_det(b, state, B_union_vec, v_union_vec, t)
+val = V_function_det(b, state, BB_vec, vv_vec, t)
 
+###############################################################################
+state = 3
+a = 1
+b = 1.2
+t = 3
 B_v_S, sto_B, sto_v = compute_Q_function_stochastic_data(BB_vec[t], vv_vec[t], states, actions, P[1], C[1], R[1], Γ[1]);
-Q_i_a_b = Q_function_stochastic(b, i, a, sto_B, sto_v)
+B_v_S[state,a]
+sto_B[state,a]
+sto_v[state,a]
+Q_i_a_b = Q_function_stochastic(b, state, a, sto_B, sto_v)
 
+Q_star, sto_B_V, sto_v_V = compute_V_function_stochastic_data_state(sto_B[state,:], sto_v[state,:])
+Q_star
+sto_B_V
+sto_v_V
 
+###
+Q_star = Vector{Vector{Tuple{Int64, Float64, Float64}}}(undef, length(states))
+sto_B_V = Vector{Vector{Float64}}(undef, length(states))
+sto_v_V = Vector{Vector{Float64}}(undef, length(states))
+for i in states
+    Q_star[i], sto_B_V[i], sto_v_V[i] = compute_V_function_stochastic_data_state(sto_B[i,:], sto_v[i,:])
+end
+Q_star[1]
+sto_B_V[1]
+sto_v_V[1]
+
+###
+Q_star_vec, sto_B_V_vec, sto_v_V_vec, sto_B_vec, sto_v_vec = compute_V_function_stochastic_data(BB_vec, vv_vec, states, actions, P[1], C[1], R[1], Γ[1], T);
+Q_star_vec[1]
+sto_B_V_vec[2][2]
+sto_v_V_vec[2][2]
+
+for t = 1:T
+    println("sto_B_V (s=$state, t=$t) = \n$(sto_B_V_vec[t][state])")
+    println("sto_v_V (s=$state, t=$t) = \n$(sto_v_V_vec[t][state])\n")
+    # println("sto_B (s=$state, t=$t) = \n$(sto_B_vec[t][state,:])")
+    # println("sto_v (s=$state, t=$t) = \n$(sto_v_vec[t][state,:])\n")
+    if t == T
+        println("sto_B_V (s=$state, t=$(t+1)) = \n$(sto_B_V_vec[t+1][state])")
+        println("sto_v_V (s=$state, t=$(t+1)) = \n$(sto_v_V_vec[t+1][state])\n")
+    end
+end
 
 
 t = 3
 state = 1
 action = 2
 for t = 1:T
-    println("B_union (t=$t) = $(B_union_vec[t][state])\n")
-    println("v_union (t=$t) = $(v_union_vec[t][state])\n")
-    # println("B (t=$t) = $(B_vec[t][state])\n")
-    # println("v (t=$t) = $(v_vec[t][state])\n")
+    println("B_union (t=$t) = \n$(B_union_vec[t][state])\n")
+    println("v_union (t=$t) = \n$(v_union_vec[t][state])\n")
+    # println("B (t=$t) = \n$(B_vec[t][state])\n")
+    # println("v (t=$t) = \n$(v_vec[t][state])\n")
     # if t == T
-    #     println("B (t=$(t+1)) = $(B_vec[t+1][state])\n")
-    #     println("v (t=$(t+1)) = $(v_vec[t+1][state])\n")
+    #     println("B (t=$(t+1)) = \n$(B_vec[t+1][state])\n")
+    #     println("v (t=$(t+1)) = \n$(v_vec[t+1][state])\n")
     # end
 end
 
 for t in 1:T+1, i in 1:num_states
-    plot_V(i, t, BB_vec[t][i], vv_vec[t][i], save_plot=false)
+    plot_V_det(i, t, BB_vec[t][i], vv_vec[t][i], save_plot=false)
 end
 
 length(B_union_vec)
@@ -100,4 +141,4 @@ v_union_vec[end][state]
 v_vec[end][state, action]
 v_tilde_union_vec[end][state]
 
-plot_V(state, t, BB_vec[t][state], vv_vec[t][state])
+plot_V_det(state, t, BB_vec[t][state], vv_vec[t][state])
