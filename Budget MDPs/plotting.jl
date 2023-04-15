5a39f6f33a9a7f7348d0dd6e9ce6cd98f14b4231
Raul Garcia <rauljgarcia66@gmail.com>
2023-04-14 20:23:24 -0500

    Added stochastic plot version + method

--- Budget MDPs/plotting.jl
+++ Budget MDPs/plotting.jl
@@ -1,10 +1,11 @@
 using Plots
 
-function plot_V(i, t, B::Vector{Float64}, v::Vector{Float64}; save_plot=false)
+function plot_V_det(i, t, B::Vector{Float64}, v::Vector{Float64}; save_plot=false)
     # B: budgets for some state and time
     # v: values for some state and time
+    # TODO: Add action ticks to scatter to denote the action
 
-    plot(title="V(s = $i; t = $t)", xlabel="Budget", ylims = (-0.05 * maximum(v), 1.05 * maximum(v)), legend=false)  # assuming minimum(v) = 0
+    plot(title="V(s = $i; t = $t) deterministic", xlabel="Budget", ylims = (-0.05 * maximum(v), 1.05 * maximum(v)), legend=false)  # assuming minimum(v) = 0
     for i in 1:(length(B)-1)
         plot!([B[i]; B[i+1]], [v[i]; v[i]]) # ls = :dash)
         scatter!([B[i]], [v[i]])
@@ -12,25 +13,46 @@ function plot_V(i, t, B::Vector{Float64}, v::Vector{Float64}; save_plot=false)
     display(scatter!([B[end]], [v[end]]))
 
     if save_plot
-        png("V at i $i t $t")
+        png("V at i $i t $t det")
+    end
+end
+
+
+# TODO: Fix this method for when B and v are B_union and v_union
+function plot_V_det(i, t, B::Vector{Pair{Int, Float64}}, v::Vector{Pair{Int, Float64}}; save_plot=false)
+    # B: budgets for some state and time
+    # v: values for some state and time
+    # TODO: Add action ticks to scatter to denote the action
+
+    plot(title="V(s = $i; t = $t) deterministic", xlabel="Budget", ylims = (-0.05 * maximum(v), 1.05 * maximum(v)), legend=false)  # assuming minimum(v) = 0
+    for i in 1:(length(B)-1)
+        plot!([B[i]; B[i+1]], [v[i]; v[i]]) # ls = :dash)
+        scatter!([B[i]], [v[i]])
+    end
+    display(scatter!([B[end]], [v[end]]))
+
+    if save_plot
+        png("V at i $i t $t det")
+    end
+end
+
+
+function plot_V_stochastic(i, t, B::Vector{Float64}, v::Vector{Float64}; save_plot=false)
+    # B: budgets for some state and time
+    # v: values for some state and time
+    # TODO: Add action ticks to scatter to denote the action
+
+    plot(title="V(s = $i; t = $t) stochastic", xlabel="Budget", ylims = (-0.05 * maximum(v), 1.05 * maximum(v)), legend=false)  # assuming minimum(v) = 0
+    for i in 1:(length(B)-1)
+        plot!([B[i]; B[i+1]], [v[i]; v[i+1]]) # ls = :dash)
+        scatter!([B[i]], [v[i]])
+    end
+    display(scatter!([B[end]], [v[end]]))
+
+    if save_plot
+        png("V at i $i t $t stochastic")
     end
 end
 
 
 # # TODO: Create color coded function where lines correspond to actions
-# # Difficulty lies in getting the axes to just show unique colors for the lines
-# function plot_V(i, t, B::Vector{Pair{T,Float64}}, v::Vector{Pair{T,Float64}}; save_plot=false) where {T}
-#     # B: budgets for some state and time as (action => budget) pairs
-#     # v: values for some state and time as (action => value) pairs
-
-#     plot(title="V(s = $i; t = $t)", xlabel="Budget", ylims = (-0.05 * maximum(v), 1.05 * maximum(v)), legend=false)  # assuming minimum(v) = 0
-#     for i in 1:(length(B)-1)
-#         plot!([B[i]; B[i+1]], [v[i]; v[i]]) # ls = :dash)
-#         scatter!([B[i]], [v[i]])
-#     end
-#     display(scatter!([B[end]], [v[end]]))
-
-#     if save_plot
-#         png("V at i $i t $t")
-#     end
-# end
