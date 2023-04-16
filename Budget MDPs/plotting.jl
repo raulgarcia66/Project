72b8ce7cf5a1c5c38afa881a61efa73e4934fb1b
Raul Garcia <rauljgarcia66@gmail.com>
2023-04-16 11:48:27 -0500

    Added methods not need time period

--- Budget MDPs/plotting.jl
+++ Budget MDPs/plotting.jl
@@ -28,8 +28,8 @@ end
 
 
 function plot_V_stochastic(i, t::Int, B::Vector{Float64}, v::Vector{Float64}; save_plot::Bool=false)
-    # B: budgets for some state and time
-    # v: values for some state and time
+    # B: budgets for state i at time t
+    # v: values for state i at time t
 
     plot(title="V(s = $i; t = $t) stochastic", xlabel="Budget", ylims = (-0.05 * maximum(v), 1.05 * maximum(v)), legend=false)  # assuming minimum(v) = 0
     for j in 1:(length(B)-1)
@@ -43,5 +43,55 @@ function plot_V_stochastic(i, t::Int, B::Vector{Float64}, v::Vector{Float64}; sa
     end
 end
 
+
+function plot_V_stochastic(i, B::Vector{Float64}, v::Vector{Float64}; save_plot::Bool=false)
+    # B: budgets for state i at some time
+    # v: values for state i at some time
+
+    plot(title="V(s = $i) stochastic", xlabel="Budget", ylims = (-0.05 * maximum(v), 1.05 * maximum(v)), legend=false)  # assuming minimum(v) = 0
+    for j in 1:(length(B)-1)
+        plot!([B[j]; B[j+1]], [v[j]; v[j+1]]) # ls = :dash)
+        scatter!([B[j]], [v[j]])
+    end
+    display(scatter!([B[end]], [v[end]]))
+
+    if save_plot
+        png("V at i $i stochastic")
+    end
+end
+
+
+function plot_Q_stochastic(i, a, t, B::Vector{Float64}, v::Vector{Float64}; save_plot::Bool=false)
+    # B: budgets for state i and action a at time t
+    # v: values for state i and action a at time t
+
+    plot(title="Q(s = $i; a = $a; t = $t) stochastic", xlabel="Budget", ylims = (-0.05 * maximum(v), 1.05 * maximum(v)), legend=false)  # assuming minimum(v) = 0
+    for j in 1:(length(B)-1)
+        plot!([B[j]; B[j+1]], [v[j]; v[j+1]]) # ls = :dash)
+        scatter!([B[j]], [v[j]])
+    end
+    display(scatter!([B[end]], [v[end]]))
+
+    if save_plot
+        png("Q at i $i a = $a t = $t stochastic")
+    end
+end
+
+
+function plot_Q_stochastic(i, a, B::Vector{Float64}, v::Vector{Float64}; save_plot::Bool=false)
+    # B: budgets for state i and action a at some time
+    # v: values for state i and action a at some time
+
+    plot(title="Q(s = $i; a = $a) stochastic", xlabel="Budget", ylims = (-0.05 * maximum(v), 1.05 * maximum(v)), legend=false)  # assuming minimum(v) = 0
+    for j in 1:(length(B)-1)
+        plot!([B[j]; B[j+1]], [v[j]; v[j+1]]) # ls = :dash)
+        scatter!([B[j]], [v[j]])
+    end
+    display(scatter!([B[end]], [v[end]]))
+
+    if save_plot
+        png("Q at i $i a = $a stochastic")
+    end
+end
 
 # TODO: Create color coded function where lines correspond to actions
