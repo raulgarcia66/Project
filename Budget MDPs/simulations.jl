7736e6a7630cb159f019de89a939d99c322cf1c6
Raul Garcia <rauljgarcia66@gmail.com>
2023-04-19 00:28:20 -0500

    initial simulations

--- Budget MDPs/simulations.jl
+++ Budget MDPs/simulations.jl
@@ -4,10 +4,12 @@ using Gurobi
 import Random
 using Pipe
 include("./generate_random_parameters.jl")
-include("./solve.jl")
+include("./BMDP functions.jl")
+include("./budget allocation.jl")
+include("./simulation functions.jl")
 include("./plotting.jl")
 
-num_subs = 2   # number of classes of subprocesses
+num_subs = 3   # number of classes of subprocesses
 B_max = 10   # max budget at any states
 global_budget = 10   # budget to be distributed among subclasses
 
@@ -30,27 +32,54 @@ U_terminal = zeros(num_states)  # reward when no action is taken (e.g., at end o
 
 # Discount factor
 Random.seed!(1)
-Γ = 1 .- rand(num_sub)*0.05
+Γ = 1 .- rand(num_subs)*0.05
 
 ###############################################################################
 ##### Deterministic
 T = 3    # time horizon ≈ 6 weeks of treatment
 
-B_union_vec_subs = Vector{Vector{Vector{Pair{Int, Float64}}}}[]
-v_union_vec_subs = Vector{Vector{Vector{Pair{Int, Float64}}}}[]
-BB_vec_subs = Vector{Vector{Vector{Float64}}}[]
-vv_vec_subs = Vector{Vector{Vector{Float64}}}[]
+B_union_vec_subs, v_union_vec_subs, σ_union_vec_subs, BB_vec_subs, vv_vec_subs, σ_vec_subs = compute_deterministic_data_multiple(num_subs, states, actions, B_max, P, C, R, Γ, T)
 
-for sub = 1:num_subs
-    B_union_vec, v_union_vec, BB_vec, vv_vec = compute_deterministic_data(states, actions, B_max, P[sub], C[sub], R[sub], Γ[sub], T);
-    push!(B_union_vec_subs, copy(B_union_vec))
-    push!(v_union_vec_subs, copy(v_union_vec))
-    push!(BB_vec_subs, copy(BB_vec))
-    push!(vv_vec_subs, copy(vv_vec))
-end
-
-init_state_subs = [1,2]   # arbitrary
+init_state_subs = rand(1:3,3)   # arbitrary
 
 x_vals, obj_val = solve_UBAP(num_subs, init_state_subs, global_budget, BB_vec_subs, vv_vec_subs)
-indices = extract_budget_index(x_vals, num_subs)
-# @show x_vals[2,:]
+index_subs = extract_budget_index(x_vals, num_subs)
+init_budget_subs = map(sub -> BB_vec_subs[sub][1][init_state_subs[sub]][index_subs[sub]], 1:num_subs)
+
+budgets, actions_taken, states_visited, values_gained, costs_incurred = simulate_det(init_budget_subs, init_state_subs, 
+                                                                            B_union_vec_subs, v_union_vec_subs, σ_union_vec_subs, BB_vec_subs, P, C, R, Γ, T);
+
+actions_taken
+budgets
+states_visited
+
+costs_incurred
+@pipe sum(costs_incurred) |> sum(_)
+values_gained
+@pipe sum(values_gained) |> sum(_)
+obj_val
+
+
+budgets_vec = []
+actions_taken_vec = []
+states_visited_vec =[]
+values_gained_vec = []
+costs_incurred_vec = []
+runs = 1000
+for _ = 1:runs
+    budgets, actions_taken, states_visited, values_gained, costs_incurred = simulate_det(init_budget_subs, init_state_subs, 
+                                                                            B_union_vec_subs, v_union_vec_subs, σ_union_vec_subs, BB_vec_subs, P, C, R, Γ, T)
+    push!(budgets_vec, budgets)
+    push!(actions_taken_vec, actions_taken)
+    push!(states_visited_vec, states_visited)
+    push!(values_gained_vec, values_gained)
+    push!(costs_incurred_vec, costs_incurred)
+end
+
+costs_vec = map(c_i -> sum(sum(c_i)), costs_incurred_vec)
+values_vec = map(v_g -> sum(sum(v_g)), values_gained_vec)
+using Statistics
+mean(costs_vec)   # 16.893
+sum(init_budget_subs)   # 9.996
+mean(values_vec)   # 30.383
+obj_val   # 30.494
